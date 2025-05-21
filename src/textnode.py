88d3ea0e1a746407dc397824d97f3ff82852d71b
Lydex90 <ozzwizard_91@hotmail.com>
2025-05-21 22:54:06 +0200

    Chapters 1 and 2 of the static site generator project

---
+++ src/textnode.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+from htmlnode import LeafNode
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    CODE = "`Code text`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
+
+def text_node_to_html_node(text_node):
+        if not isinstance(text_node.text_type, TextType):
+            raise ValueError("not valid TextType")
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(None, text_node.text)
+            case TextType.BOLD:
+                return LeafNode("b", text_node.text)
+            case TextType.ITALIC:
+                return LeafNode("i", text_node.text)
+            case TextType.CODE:
+                return LeafNode("code", text_node.text)
+            case TextType.LINK:
+                return LeafNode("a", text_node.text, {"href": text_node.url})
+            case TextType.IMAGE:
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other_node):
+        return (self.text == other_node.text) and (self.text_type == other_node.text_type) and (self.url == other_node.url)
+    
+    #def text_node_to_html_node(text_node):
+    #    if text_node.text_type not in TextType:
+    #        raise Exception("not valid TextType")
+    #    match text_node.text_type:
+    #        case TextType.TEXT:
+    #            return LeafNode(None, text_node.text)
+    #        case TextType.BOLD:
+    #            return LeafNode("b", text_node.text)
+    #        case TextNode.ITALIC:
+    #            return LeafNode("i", text_node.text)
+    #        case TextNode.CODE:
+    #            return LeafNode("code", text_node.text)
+    #        case TextNode.LINK:
+    #            return LeafNode("a", text_node.text, {"href": text_node.url})
+    #        case TextNode.IMAGE:
+    #            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
